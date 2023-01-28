29d28fc9e2275b9a7b7b20c49fca1473a482e26f
KonradK <konrad.0808.k@gmail.com>
2023-01-28 01:48:36 +0100

    Update fonts and center content.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var css = document.querySelector("h3");
+var css = document.querySelector("h4");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
@@ -6,14 +6,15 @@ var body = document.getElementById("gradient");
 setGradient();
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+	body.style.background =
+		"linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ")";
 
-	css.textContent = body.style.background + ";";
+	css.textContent = "Colors: " + body.style.background.slice(26).slice(0, -1);
+	console.log("LOG:", css.textContent);
 }
 
 color1.addEventListener("input", setGradient);
