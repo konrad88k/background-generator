4e50fd7afc576480e14033da0b0bf3a8daab3565
konrad88k <konrad.0808.k@gmail.com>
2023-03-10 22:26:15 +0100

    Update project before add to portfolio.

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-var css = document.querySelector("h4");
+var gradient = document.getElementById("gradient");
+var border = document.querySelector(".border-gradient");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var rgb1 = document.querySelector("#desc1");
+var rgb2 = document.querySelector("#desc2");
 
 setGradient();
 
 function setGradient() {
-	body.style.background =
+	gradient.style.background =
 		"linear-gradient(to right, "
 		+ color1.value
 		+ ", "
 		+ color2.value
 		+ ")";
 
-	css.textContent = "Colors: " + body.style.background.slice(26).slice(0, -1);
-	console.log("LOG:", css.textContent);
+	border.style.background =
+		"linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ")";
+
+	rgb1.textContent = gradient.style.background.slice(26).slice(0, -1);
+	rgb2.textContent = gradient.style.background.slice(26).slice(0, -1);
+
+	console.log("GRADIENT:", gradient.style.background);
 }
 
 color1.addEventListener("input", setGradient);
